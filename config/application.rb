47c89c58076aed70b1671c08e01119d79be31ab1
Alejandro Alzate <aalzate@tpaga.co>
2023-01-20 04:15:10 -0500

    Error Heroku app

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Boilerplate
   class Application < Rails::Application
     config.load_defaults 6.1
     config.action_view.form_with_generates_remote_forms = true
+    config.secret_key_base = 'bd9f35adc889479a6142f34c0e40f2f5f779d3c8ff30ae535761957ba331cc68d6dcf695ee110535bbd7b7677221b35b17427730b4a5331b52935cfb8e209d14'
   end
 end
