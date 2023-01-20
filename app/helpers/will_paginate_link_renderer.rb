4a72739ac07169407bdda0ac93804dc44eab1a98
Alejandro Alzate <aalzate@tpaga.co>
2023-01-20 01:24:03 -0500

    Error Heroku app

--- app/helpers/will_paginate_link_renderer.rb
+++ app/helpers/will_paginate_link_renderer.rb
@@ -1,2 +1,9 @@
 # frozen_string_literal: true
 
+class WillPaginateLinkRenderer < WillPaginate::LinkRenderer
+  def link(text, target, attributes = {})
+    attributes['data-remote'] = true
+    attributes['class'] = 'pagination'
+    super
+  end
+end
